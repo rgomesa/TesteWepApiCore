55534c6d8c0e23390eab5a6973f9d3924c452d45
Rosangela Gomes <r.gomesa@hotmail.com>
2017-02-11 22:40:51 -0200

    Commit inicial

---
+++ src/AppServices/Controllers/ValuesController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AppServices.Models;
+
+namespace AppServices.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+
+        
+
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            Produto p = new Produto();
+            return p.GetAll();
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public Produto Get(int id)
+        {
+            Produto p = new Produto();
+            return p.Get(id);
+        }
+
+        // GET api/values/5
+        [HttpGet("{usuario}/{senha}")]
+        public Boolean Get(string usuario, string senha )
+        {
+            return false;
+        }
+
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ src/AppServices/Models/Produto.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppServices.Models
+{
+    public class Produto
+    {
+        public int id;
+        public string nome;
+        public string marca;
+        public double preco;
+
+        private List<Produto> lista = new List<Produto>();
+
+        public Produto()
+        {
+
+            
+        }
+
+
+        public List<Produto> GetAll ()
+        {
+
+
+            lista.Clear();
+            lista.Add(new Produto() { id = 1, nome = "Bombril", marca = "PeG", preco = 100 });
+            lista.Add(new Produto() { id = 2, nome = "Cafe Voluto", marca = "Nespresso", preco = 35.90 });
+            lista.Add(new Produto() { id = 3, nome = "Delirium Tremens", marca = "Deliriuns", preco = 100 });
+
+            return this.lista;
+
+        }
+
+        public Produto Get(int id)
+        {
+
+
+            return new Produto() { id = 1, nome = "Bombril", marca = "PeG", preco = 100 };
+        }
+
+    }
+}
